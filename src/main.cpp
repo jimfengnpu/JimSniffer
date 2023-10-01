ae022c2e0c9f8c27b026e3093f80c0ce202dcd8b
jimfengnpu <jimfelpos@163.com>
2023-10-01 17:34:01 +0800

    remake py

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "sniffer_window.h"
-
-
-int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    JmSniffer mainWindow(nullptr);
-    return QApplication::exec();
-}
